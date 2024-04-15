0e6ad27cc5edcae4c83f3c7c10160bcc0f50b0e1
mg0806 <manohargupta0806@gmail.com>
2024-04-15 23:24:23 +0530

    update Hireme.js and projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -133,7 +133,7 @@ const projects = () => {
         <Layout className=" pt-16">
           <AnimatedText
             text="Crafting Digital Experiences Excellently."
-            className=" mb-16 mt-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:text-4xl"
+            className=" mb-16 mt-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
           />
 
           <div className=" grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
